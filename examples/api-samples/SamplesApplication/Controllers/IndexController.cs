21e0629160afefe764e3fa3941a30d81fcc5225d
Sergey Zhuravel <sergey.zhuravel@groupdocs.com>
2013-03-07 08:13:39 +0200

    Added template for api samples application

---
+++ examples/api-samples/SamplesApplication/Controllers/IndexController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Configuration;
+using System.Web.Mvc;
+using Groupdocs.Sdk;
+
+namespace SamplesApp.Controllers
+{
+    public class SamplesController : Controller
+    {
+        public ActionResult Index(String guid)
+        {
+            // Pass GUID of the document as model.
+            return View("Index", null, guid);
+        }
+
+        public ActionResult Sample01(String guid)
+        {
+            int[] test = new int[3];
+            test[0] = 1;
+            test[1] = 2;
+            test[2] = 3;
+            // Pass GUID of the document as model.
+            return View("sample01", null, test);
+        }
+
+        [HttpPost]
+        public ActionResult Upload()
+        {
+            // Read settings from the configuration file.
+            var baseAddress = ConfigurationManager.AppSettings["baseAddress"];
+            var userId = ConfigurationManager.AppSettings["userId"];
+            var privateKey = ConfigurationManager.AppSettings["privateKey"];
+
+            // Create service for uploading file to Groupdocs account
+            var service = new GroupdocsService(baseAddress, userId, privateKey);
+
+            // Iterate through the uploaded files. Right now we have just one uploaded file at a time.
+            foreach (string inputTagName in Request.Files)
+            {
+                var file = Request.Files[inputTagName];
+                // Check that file is not fake.
+                if (file.ContentLength > 0)
+                {
+                    // Upload file with empty description.
+                    var result = service.UploadFile(file.FileName, String.Empty, file.InputStream);
+                    // Redirect to viewer with received GUID.
+                    return RedirectToAction("Index", new {guid = result.Guid});
+                }
+            }
+
+            // If file was not uploaded redirect to the current page.
+            return RedirectToAction("Index");
+        }
+    }
+}
